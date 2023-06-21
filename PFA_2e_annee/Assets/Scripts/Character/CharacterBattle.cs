11c3459e2031f34eee95d0efa4a834bf4819381f
Barthelemi Rapeaux <rapeauxb@gmail.com>
2023-06-21 16:08:10 +0200

    RETAKE: stuff

--- PFA_2e_annee/Assets/Scripts/Character/CharacterBattle.cs
+++ PFA_2e_annee/Assets/Scripts/Character/CharacterBattle.cs
@@ -760,82 +760,84 @@ public class CharacterBattle : MonoBehaviour
             }
         }
 
+        if (target.CharacterStats.Health.CurrentValue > 0)
+        {
+            if (action.StrengthModifier.Value != 0)
+            {
+                StatModifier modifier = new StatModifier(action.StrengthModifier.Value, action.StrengthModifier.Type, this);
+                target.CharacterStats.Strength.AddModifier(modifier);
+            }
+            if (action.AgilityModifier.Value != 0)
+            {
+                StatModifier modifier = new StatModifier(action.AgilityModifier.Value, action.AgilityModifier.Type, this);
+                target.CharacterStats.Agility.AddModifier(modifier);
+            }
+            if (action.IntelligenceModifier.Value != 0)
+            {
+                StatModifier modifier = new StatModifier(action.IntelligenceModifier.Value, action.IntelligenceModifier.Type, this);
+                target.CharacterStats.Intelligence.AddModifier(modifier);
+            }
+            if (action.ConstitutionModifier.Value != 0)
+            {
+                StatModifier modifier = new StatModifier(action.ConstitutionModifier.Value, action.ConstitutionModifier.Type, this);
+                target.CharacterStats.Constitution.AddModifier(modifier);
+            }
+            if (action.VitalityModifier.Value != 0)
+            {
+                StatModifier modifier = new StatModifier(action.VitalityModifier.Value, action.VitalityModifier.Type, this);
+                target.CharacterStats.Vitality.AddModifier(modifier);
+            }
+            if (action.LuckModifier.Value != 0)
+            {
+                StatModifier modifier = new StatModifier(action.LuckModifier.Value, action.LuckModifier.Type, this);
+                target.CharacterStats.Luck.AddModifier(modifier);
+            }
+            if (action.HealthModifier.Value != 0)
+            {
+                StatModifier modifier = new StatModifier(action.HealthModifier.Value, action.HealthModifier.Type, this);
+                target.CharacterStats.Health.AddModifier(modifier);
+            }
+            if (action.EtherModifier.Value != 0)
+            {
+                StatModifier modifier = new StatModifier(action.EtherModifier.Value, action.EtherModifier.Type, this);
+                target.CharacterStats.Ether.AddModifier(modifier);
+            }
+            if (action.SpeedModifier.Value != 0)
+            {
+                StatModifier modifier = new StatModifier(action.SpeedModifier.Value, action.SpeedModifier.Type, this);
+                target.CharacterStats.Speed.AddModifier(modifier);
+            }
+            if (action.PhysDMGModifier.Value != 0)
+            {
+                StatModifier modifier = new StatModifier(action.PhysDMGModifier.Value, action.PhysDMGModifier.Type, this);
+                target.CharacterStats.PhysicalDamage.AddModifier(modifier);
+            }
+            if (action.PhysRESModifier.Value != 0)
+            {
+                StatModifier modifier = new StatModifier(action.PhysRESModifier.Value, action.PhysRESModifier.Type, this);
+                target.CharacterStats.PhysicalResistance.AddModifier(modifier);
+            }
+            if (action.MagDMGModifier.Value != 0)
+            {
+                StatModifier modifier = new StatModifier(action.MagDMGModifier.Value, action.MagDMGModifier.Type, this);
+                target.CharacterStats.MagicalDamage.AddModifier(modifier);
+            }
+            if (action.MagRESModifier.Value != 0)
+            {
+                StatModifier modifier = new StatModifier(action.MagRESModifier.Value, action.MagRESModifier.Type, this);
+                target.CharacterStats.MagicalResistance.AddModifier(modifier);
+            }
 
-        if (action.StrengthModifier.Value != 0)
-        {
-            StatModifier modifier = new StatModifier(action.StrengthModifier.Value, action.StrengthModifier.Type, this);
-            target.CharacterStats.Strength.AddModifier(modifier);
-        }
-        if (action.AgilityModifier.Value != 0)
-        {
-            StatModifier modifier = new StatModifier(action.AgilityModifier.Value, action.AgilityModifier.Type, this);
-            target.CharacterStats.Agility.AddModifier(modifier);
-        }
-        if (action.IntelligenceModifier.Value != 0)
-        {
-            StatModifier modifier = new StatModifier(action.IntelligenceModifier.Value, action.IntelligenceModifier.Type, this);
-            target.CharacterStats.Intelligence.AddModifier(modifier);
-        }
-        if (action.ConstitutionModifier.Value != 0)
-        {
-            StatModifier modifier = new StatModifier(action.ConstitutionModifier.Value, action.ConstitutionModifier.Type, this);
-            target.CharacterStats.Constitution.AddModifier(modifier);
-        }
-        if (action.VitalityModifier.Value != 0)
-        {
-            StatModifier modifier = new StatModifier(action.VitalityModifier.Value, action.VitalityModifier.Type, this);
-            target.CharacterStats.Vitality.AddModifier(modifier);
-        }
-        if (action.LuckModifier.Value != 0)
-        {
-            StatModifier modifier = new StatModifier(action.LuckModifier.Value, action.LuckModifier.Type, this);
-            target.CharacterStats.Luck.AddModifier(modifier);
-        }
-        if (action.HealthModifier.Value != 0)
-        {
-            StatModifier modifier = new StatModifier(action.HealthModifier.Value, action.HealthModifier.Type, this);
-            target.CharacterStats.Health.AddModifier(modifier);
-        }
-        if (action.EtherModifier.Value != 0)
-        {
-            StatModifier modifier = new StatModifier(action.EtherModifier.Value, action.EtherModifier.Type, this);
-            target.CharacterStats.Ether.AddModifier(modifier);
-        }
-        if (action.SpeedModifier.Value != 0)
-        {
-            StatModifier modifier = new StatModifier(action.SpeedModifier.Value, action.SpeedModifier.Type, this);
-            target.CharacterStats.Speed.AddModifier(modifier);
-        }
-        if (action.PhysDMGModifier.Value != 0)
-        {
-            StatModifier modifier = new StatModifier(action.PhysDMGModifier.Value, action.PhysDMGModifier.Type, this);
-            target.CharacterStats.PhysicalDamage.AddModifier(modifier);
-        }
-        if (action.PhysRESModifier.Value != 0)
-        {
-            StatModifier modifier = new StatModifier(action.PhysRESModifier.Value, action.PhysRESModifier.Type, this);
-            target.CharacterStats.PhysicalResistance.AddModifier(modifier);
-        }
-        if (action.MagDMGModifier.Value != 0)
-        {
-            StatModifier modifier = new StatModifier(action.MagDMGModifier.Value, action.MagDMGModifier.Type, this);
-            target.CharacterStats.MagicalDamage.AddModifier(modifier);
-        }
-        if (action.MagRESModifier.Value != 0)
-        {
-            StatModifier modifier = new StatModifier(action.MagRESModifier.Value, action.MagRESModifier.Type, this);
-            target.CharacterStats.MagicalResistance.AddModifier(modifier);
-        }
-
-        if (action.TemperatureChange < 0)
-        {
-            target.CharacterStats.Temperature.Damage(Mathf.Abs(action.TemperatureChange));
-            target.CharacterStateHandler.CheckTemperatureTransitions();
-        }
-        else if (action.TemperatureChange > 0)
-        {
-            target.CharacterStats.Temperature.Heal(action.TemperatureChange);
-            target.CharacterStateHandler.CheckTemperatureTransitions();
+            if (action.TemperatureChange < 0)
+            {
+                target.CharacterStats.Temperature.Damage(Mathf.Abs(action.TemperatureChange));
+                target.CharacterStateHandler.CheckTemperatureTransitions();
+            }
+            else if (action.TemperatureChange > 0)
+            {
+                target.CharacterStats.Temperature.Heal(action.TemperatureChange);
+                target.CharacterStateHandler.CheckTemperatureTransitions();
+            }
         }
     }
 
--- PFA_2e_annee/Assets/Scripts/Character/CharacterStateHandler.cs
+++ PFA_2e_annee/Assets/Scripts/Character/CharacterStateHandler.cs
@@ -17,6 +17,7 @@ public class CharacterStateHandler : MonoBehaviour
     public event CharacterTypeStateEvent TransitionedFromTo = null;
 
     public CharacterStats CharacterStats;
+    public CharacterBattle Battle;
 
     public CharacterTypeState StartingState;
 
@@ -28,6 +29,14 @@ public class CharacterStateHandler : MonoBehaviour
     public GameObject LiquidCharacterMesh;
     public GameObject GasCharacterMesh;
 
+    public CharacterAnimatorHandler AnimatorHandler;
+
+    public ParticleSystem VFX_SolidTransitionFlash;
+    public ParticleSystem VFX_LiquidTransitionFlash;
+    public ParticleSystem VFX_GasTransitionFlash;
+    private IEnumerator _currentFlashCoroutine;
+    private Renderer[] _renderers;
+
     public CharacterTypeState CharacterTypeState
     {
         get
@@ -37,12 +46,18 @@ public class CharacterStateHandler : MonoBehaviour
         set
         {
             if (_internalState == value) return;
-            TransitionToState(value);
+            Flash(1f, 100f);
+            AnimatorHandler.PlayAnimThenAction("TransitionIn", () =>
+            {
+                TransitionToState(value);
+            });
         }
     }
 
     private void Awake()
     {
+        _renderers = GetComponentsInChildren<Renderer>(true);
+
         CharacterTypeState = StartingState;
         TransitionToState(StartingState);
     }
@@ -119,18 +134,29 @@ public class CharacterStateHandler : MonoBehaviour
                 break;
             case CharacterTypeState.Solid:
                 SolidCharacterMesh.SetActive(true);
+                AnimatorHandler = SolidCharacterMesh.GetComponent<CharacterAnimatorHandler>();
+                Battle.CharacterAnimatorHandler = AnimatorHandler;
+                VFX_SolidTransitionFlash.Play();
                 break;
             case CharacterTypeState.Liquid:
                 LiquidCharacterMesh.SetActive(true);
+                AnimatorHandler = LiquidCharacterMesh.GetComponent<CharacterAnimatorHandler>();
+                Battle.CharacterAnimatorHandler = AnimatorHandler;
+                VFX_LiquidTransitionFlash.Play();
                 break;
             case CharacterTypeState.Gas:
                 GasCharacterMesh.SetActive(true);
+                AnimatorHandler = GasCharacterMesh.GetComponent<CharacterAnimatorHandler>();
+                Battle.CharacterAnimatorHandler = AnimatorHandler;
+                VFX_GasTransitionFlash.Play();
                 break;
             case CharacterTypeState.TriplePoint:
                 break;
             default:
                 break;
         }
+        AnimatorHandler.PlayAnimThenAction("TransitionOut", null);
+        Flash(100f, 1f, false);
     }
 
     private void ForceTransitionNoCalls(CharacterTypeState toState)
@@ -155,11 +181,21 @@ public class CharacterStateHandler : MonoBehaviour
 
         if (currentIndex > PossibleStates.Count - 1)
         {
-            TransitionToState(PossibleStates[0]);
+            //Play transitionIn animation. At the end of the animation, play transitionOut animation.
+            Flash(1f, 100f);
+            AnimatorHandler.PlayAnimThenAction("TransitionIn", () =>
+            {
+                TransitionToState(PossibleStates[0]);
+            });
+
         }
         else
         {
-            TransitionToState(PossibleStates[currentIndex]);
+            Flash(1f, 100f);
+            AnimatorHandler.PlayAnimThenAction("TransitionIn", () =>
+            {
+                TransitionToState(PossibleStates[currentIndex]);
+            });
         }
     }
 
@@ -178,16 +214,74 @@ public class CharacterStateHandler : MonoBehaviour
 
         if (currentIndex < 0)
         {
-            TransitionToState(PossibleStates[PossibleStates.Count-1]);
+            Flash(1f, 100f);
+            AnimatorHandler.PlayAnimThenAction("TransitionIn", () =>
+            {
+                TransitionToState(PossibleStates[PossibleStates.Count - 1]);
+            });
         }
         else
         {
-            TransitionToState(PossibleStates[currentIndex]);
+            Flash(1f, 100f);
+            AnimatorHandler.PlayAnimThenAction("TransitionIn", () =>
+            {
+                TransitionToState(PossibleStates[currentIndex]);
+            });
         }
     }
 
     public void SwitchStateTo(CharacterTypeState toState)
     {
         CharacterTypeState = toState;
+        Flash(1f, 100f);
+        AnimatorHandler.PlayAnimThenAction("TransitionIn", () =>
+        {
+            TransitionToState(toState);
+        });
+    }
+
+    private void Flash(float fromIntensity, float toIntensity, bool revertToFrom = true)
+    {
+        if (_currentFlashCoroutine != null) StopCoroutine(_currentFlashCoroutine);
+        _currentFlashCoroutine = MeshFlash(fromIntensity, toIntensity, .4f, revertToFrom);
+        StartCoroutine(MeshFlash(fromIntensity, toIntensity, .4f, revertToFrom));
+    }
+
+    private IEnumerator MeshFlash(float fromIntensity, float toIntensity, float overTime, bool revertToFrom = true)
+    {
+        float timer = 0f;
+
+        foreach (Renderer renderer in _renderers)
+        {
+            renderer.material.SetFloat("_albedoIntensity", fromIntensity);
+        }
+
+        while (timer < overTime)
+        {
+            timer += Time.deltaTime;
+            float lerpdIntensity = Mathf.Lerp(fromIntensity, toIntensity, timer / overTime);
+            foreach (Renderer renderer in _renderers)
+            {
+                renderer.material.SetFloat("_albedoIntensity", lerpdIntensity);
+            }
+            yield return null;
+        }
+
+        //Revert to original colors
+        if (revertToFrom)
+        {
+            foreach (Renderer renderer in _renderers)
+            {
+                renderer.material.SetFloat("_albedoIntensity", fromIntensity);
+            }
+        }
+        else
+        {
+            foreach (Renderer renderer in _renderers)
+            {
+                renderer.material.SetFloat("_albedoIntensity", toIntensity);
+            }
+        }
+
     }
 }
